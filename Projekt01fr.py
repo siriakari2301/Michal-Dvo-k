64c6818ad5eabdacaef85a0d971edbf10784895e
siriakari2301 <153946825+siriakari2301@users.noreply.github.com>
2024-11-14 20:28:55 +0100

    Update Projekt01fr.py
    Upravený skript - odstraněny duplicity z kopirovani.

--- Projekt01fr.py
+++ Projekt01fr.py
@@ -2,8 +2,10 @@
 Projekt 1 - Datový analytik s pythonem - 17/10/2024
 autor: Michal Dvořák
 e-mail: dvmichal@gmail.com
+Původní text jsem kopiroval z e-mailu, protože jsem nemohl otevřít - ochrana systému, přípona "py",
+takže se random něco zkopírovalo vícekrát. Obecný text nahrazen zadáním. FR protože EN je ordinerní...
 """
-# Registrovaní uživatelé
+# 1. Registrovaní uživatelé
 registered_users = {
     "bob": "123",
     "ann": "pass123",
@@ -11,34 +13,47 @@ registered_users = {
     "liz": "pass123"
 }
 
-# Tři texty pro analýzu
-# Registrovaní uživatelé
-registered_users = {
-    "bob": "123",
-    "ann": "pass123",
-    "mike": "password123",
-    "liz": "pass123"
-}
-
-# Tři texty pro analýzu
+# 2. Tři texty pro analýzu
 TEXTS = [
-    """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.""",
-    """Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.""",
-    """Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."""
+'''bobSituated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
 ]
 
-# 1. Přihlašování uživatele
+# 3. Přihlašování uživatele
 username = input("nom d'utilisateur : ")
 password = input("mot de passe : ")
 
-# 2. Ověření uživatele
+# 4. Ověření uživatele
 if registered_users.get(username) == password:
     print("----------------------------------------")
     print(f"Bienvenue dans l'application, {username}")
     print(f"Nous avons {len(TEXTS)} textes à analyser.")
     print("----------------------------------------")
 
-    # 3. Výběr textu pro analýzu
+    # 5. Výběr textu pro analýzu
     try:
         text_choice = int(input("Entrez un nombre entre 1 et 3 pour sélectionner : ")) - 1
         if text_choice not in range(len(TEXTS)):
@@ -48,23 +63,23 @@ if registered_users.get(username) == password:
         print("Entrée invalide, fin du programme.")
         exit()
 
-    # 4. Analýza vybraného textu
+    # 6. Analýza vybraného textu
     selected_text = TEXTS[text_choice]
     words = selected_text.split()
 
-    # Počet slov
+    # 7. Počet slov
     word_count = len(words)
 
-    # Počet slov začínajících velkým písmenem, velkých slov, malých slov
+    # 8. Počet slov začínajících velkým písmenem, velkých slov, malých slov
     titlecase_count = sum(1 for word in words if word.istitle())
     uppercase_count = sum(1 for word in words if word.isupper() and word.isalpha())
     lowercase_count = sum(1 for word in words if word.islower())
     
-    # Počet čísel (ne cifer) a součet čísel
+    # 9. Počet čísel (ne cifer) a součet čísel
     numeric_count = sum(1 for word in words if word.isdigit())
     numeric_sum = sum(int(word) for word in words if word.isdigit())
 
-    # Výpis výsledků analýzy
+    # 10. Výpis výsledků analýzy
     print("----------------------------------------")
     print(f"Il y a {word_count} mots dans le texte sélectionné.")
     print(f"Il y a {titlecase_count} mots en majuscule.")
@@ -74,13 +89,13 @@ if registered_users.get(username) == password:
     print(f"La somme de tous les nombres est {numeric_sum}")
     print("----------------------------------------")
 
-    # 5. Sloupcový graf pro četnost délek slov
+    # 11. Sloupcový graf pro četnost délek slov
     word_lengths = {}
     for word in words:
         length = len(word.strip(",.!?"))
         word_lengths[length] = word_lengths.get(length, 0) + 1
 
-    # Výpis grafu
+    # 12. Výpis grafu
     print("LEN|  OCCURRENCES  |NR.")
     print("----------------------------------------")
     for length in sorted(word_lengths):
@@ -89,69 +104,3 @@ if registered_users.get(username) == password:
 
 else:
     print("utilisateur non enregistré, fin du programme.")
-
-
-# 1. Přihlašování uživatele
-username = input("nom d'utilisateur : ")
-password = input("mot de passe : ")
-
-# 2. Ověření uživatele
-if registered_users.get(username) == password:
-    print("----------------------------------------")
-    print(f"Bienvenue dans l'application, {username}")
-    print(f"Nous avons {len(TEXTS)} textes à analyser.")
-    print("----------------------------------------")
-
-    # 3. Výběr textu pro analýzu
-    try:
-        text_choice = int(input("Entrez un nombre entre 1 et 3 pour sélectionner : ")) - 1
-        if text_choice not in range(len(TEXTS)):
-            print("Choix invalide, fin du programme.")
-            exit()
-    except ValueError:
-        print("Entrée invalide, fin du programme.")
-        exit()
-
-    # 4. Analýza vybraného textu
-    selected_text = TEXTS[text_choice]
-    words = selected_text.split()
-
-    # Počet slov
-    word_count = len(words)
-
-    # Počet slov začínajících velkým písmenem, velkých slov, malých slov
-    titlecase_count = sum(1 for word in words if word.istitle())
-    uppercase_count = sum(1 for word in words if word.isupper() and word.isalpha())
-    lowercase_count = sum(1 for word in words if word.islower())
-    
-    # Počet čísel (ne cifer) a součet čísel
-    numeric_count = sum(1 for word in words if word.isdigit())
-    numeric_sum = sum(int(word) for word in words if word.isdigit())
-
-    # Výpis výsledků analýzy
-    print("----------------------------------------")
-    print(f"Il y a {word_count} mots dans le texte sélectionné.")
-    print(f"Il y a {titlecase_count} mots en majuscule.")
-    print(f"Il y a {uppercase_count} mots entièrement en majuscules.")
-    print(f"Il y a {lowercase_count} mots en minuscules.")
-    print(f"Il y a {numeric_count} chaînes numériques.")
-    print(f"La somme de tous les nombres est {numeric_sum}")
-    print("----------------------------------------")
-
-    # 5. Sloupcový graf pro četnost délek slov
-    word_lengths = {}
-    for word in words:
-        length = len(word.strip(",.!?"))
-        word_lengths[length] = word_lengths.get(length, 0) + 1
-
-    # Výpis grafu
-    print("LEN|  OCCURRENCES  |NR.")
-    print("----------------------------------------")
-    for length in sorted(word_lengths):
-        occurrences = word_lengths[length]
-        print(f"{length:>3}|{'*' * occurrences:<15}|{occurrences}")
-
-else:
-    print("utilisateur non enregistré, fin du programme.")
-Projekt_01FR.py
-Zobrazování položky Projekt_01FR.py.
